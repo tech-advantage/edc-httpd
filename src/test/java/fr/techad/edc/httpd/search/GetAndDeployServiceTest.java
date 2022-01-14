a4cc3b83e3dc212101e95f9a7d4f8e84a71949b4
gojoise <gojoise.pub@gmail.com>
2022-01-14 15:38:18 +0100

    remove another warnings

--- src/test/java/fr/techad/edc/httpd/search/GetAndDeployServiceTest.java
+++ src/test/java/fr/techad/edc/httpd/search/GetAndDeployServiceTest.java
@@ -14,7 +14,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.annotation.Testable;
 
 import com.networknt.config.Config;
 
