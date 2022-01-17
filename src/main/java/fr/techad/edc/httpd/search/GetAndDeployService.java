3007ca40a9d9ec33eeeb1ba48444b0c6d81de148
gojoise <gojoise.pub@gmail.com>
2022-01-17 11:15:59 +0100

    simplify clean and synchronize

--- src/main/java/fr/techad/edc/httpd/search/GetAndDeployService.java
+++ src/main/java/fr/techad/edc/httpd/search/GetAndDeployService.java
@@ -3,13 +3,11 @@ package fr.techad.edc.httpd.search;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.io.filefilter.DirectoryFileFilter;
-import org.apache.commons.io.filefilter.NotFileFilter;
-import org.apache.commons.io.filefilter.TrueFileFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,28 +24,26 @@ public class GetAndDeployService {
   }
 
   public boolean processing(String name, boolean override) {
+    String docPath = config.getBase() + "/" + config.getDocFolder() + "/";
     File zip = new File(tempdirPath + "/" + name);
-    String tmpdir = "" ;
+    File tmpdir = null;
+    boolean succeed = false;
     try {
-      tmpdir= Files.createTempDirectory("unzip").toFile().getAbsolutePath();
-      unzip(tempdirPath + "/" + name, tmpdir + "/doc");
-      if(replaceOldDoc(new File(tmpdir), override)) {
+      tmpdir = Files.createTempDirectory("unzip").toFile();
+      unzip(tempdirPath + "/" + name, tmpdir.getAbsolutePath());
+      if (verifyStructure(tmpdir)) {
+        cleanPreviousDoc(tmpdir, docPath, override);
+        synchronize(tmpdir, docPath, override);
         LOGGER.info("Processing finished");
-        return zip.delete();
+        succeed = true;
       }
-      zip.delete();
-      return false;
     } catch (IOException e) {
       LOGGER.error("Error in processing operations", e);
-      zip.delete();
-      try {
-        FileUtils.deleteDirectory(new File(tmpdir));
-      } catch (IOException e1) {
-        LOGGER.error("Error in delete unzipped directory", e1);
-      }
-      return false;
     }
+    removeTempFiles(tmpdir, zip);
+    return succeed;
   }
+
   // Non multi-thread
   public boolean moveZip(File toSave, String name) throws IOException {
     if (FilenameUtils.getExtension(name).equals("zip")) {
@@ -58,6 +54,17 @@ public class GetAndDeployService {
       return false;
   }
 
+  private void removeTempFiles(File tmpdir, File zip) {
+    try {
+      if (tmpdir.exists()) {
+        FileUtils.deleteDirectory(tmpdir);
+      }
+      zip.delete();
+    } catch (IOException e) {
+      LOGGER.error("Error in removing temporary Files", e);
+    }
+  }
+
   private void unzip(String filename, String tempdirPath) throws IOException {
     LOGGER.info("Unzipping File: {} into {}", filename, tempdirPath);
     ZipFile toextract = new ZipFile(filename);
@@ -65,67 +72,43 @@ public class GetAndDeployService {
     toextract.close();
   }
 
-  private boolean replaceOldDoc(File unziped, boolean override) throws IOException {
-    String docPath = config.getBase() + "/" + config.getDocFolder() + "/";
-    if (verifyStructure(unziped)) {
-      if (override) {
-        LOGGER.info("Copying new Doc with overrinding i18n");
-      } else {
-        LOGGER.info("Copying new Doc with keeping i18n");
-      }
-      synchronize(new File(unziped.getAbsolutePath() + "/doc"), docPath, override);
-      FileUtils.deleteDirectory(unziped);
-      return true;
-    }
-    FileUtils.deleteDirectory(unziped);
-    return false;
-  }
-  private void cleanPreviousDoc(File directory, String docPath,boolean override) throws IOException {
-    List<File> directoriestoCopy = (List<File>) FileUtils.listFilesAndDirs(directory,
-        new NotFileFilter(TrueFileFilter.INSTANCE), DirectoryFileFilter.DIRECTORY);
-    List<File> directoriestoClean = (List<File>) FileUtils.listFilesAndDirs(new File(docPath),
-        new NotFileFilter(TrueFileFilter.INSTANCE), DirectoryFileFilter.DIRECTORY);
-    for (int i = directoriestoCopy.size() - 1; i > 0; i--) {
-      for (int j = directoriestoClean.size() - 1; j > 0; j--) {
-        // Get correct path
-        String path1 = directoriestoCopy.get(i).getPath();
-        String path2 = directoriestoClean.get(j).getPath();
-        String[] split1 = path1.split("doc");
-        String[] split2 = path2.split(config.getDocFolder());
-        path1 = split1[1];
-        path2 = split2[1];
-        if (override) {
-          if (path1.equals(path2)) {// test if folders are same
-            LOGGER.debug("Cleaning :{}", directoriestoClean.get(j).getCanonicalPath());
-            FileUtils.cleanDirectory(directoriestoClean.get(j));
-          }
-        } else {
-          if (path1.equals(path2) && !(directoriestoClean.get(j).getPath().contains("i18n"))) {
-            LOGGER.debug("Cleaning :{}", directoriestoClean.get(j).getCanonicalPath());
-            FileUtils.cleanDirectory(directoriestoClean.get(j));
+  private void cleanPreviousDoc(File directory, String docPath, boolean override) throws IOException {
+    if (new File(docPath).exists()) {
+      File[] directoriestoCopy = directory.listFiles(File::isDirectory);
+      File[] directoriestoClean = new File(docPath).listFiles(File::isDirectory);
+      for (File dir1 : directoriestoCopy) {
+        for (File dir2 : directoriestoClean) {
+          if (override) {
+            if (dir1.getName().equals(dir2.getName())) {// test if folders are same
+              LOGGER.debug("Cleaning :{}", dir2.getCanonicalPath());
+              FileUtils.cleanDirectory(dir2);
+            }
+          } else {
+            if (dir1.getName().equals(dir2.getName()) && !(dir2.getName().contains("i18n"))) {
+              LOGGER.debug("Cleaning :{}", dir2.getCanonicalPath());
+              FileUtils.cleanDirectory(dir2);
+            }
           }
         }
       }
     }
   }
-   private void synchronize(File directory, String docPath, boolean override) throws IOException {
+
+  private void synchronize(File directory, String docPath, boolean override) throws IOException {
     String[] extensions = { "bmp", "dib", "eps", "ico", "webp", "jpg", "jpeg", "jpe", "jif", "jfif", "jfi", "jp2",
         "j2k", "jpf", "jpx", "jpm", "mj2", "tiff", "tif", "json", "svg", "svgz", "pdf", "css", "html", "png", "txt",
-        "gif","ai" };
+        "gif", "ai" };
     List<File> filestoCopy = (List<File>) FileUtils.listFiles(directory, extensions, true);
-    if(new File(docPath).exists()) {
-      cleanPreviousDoc(directory, docPath, override);
+    if (override) {
+      LOGGER.info("Copying new Doc with overrinding i18n");
+    } else {
+      LOGGER.info("Copying new Doc with keeping i18n");
     }
     for (File f : filestoCopy) {
       String path1 = f.getPath();
-      String[] split1 = path1.split("doc");
-      if (f.getName().equals("multi-doc.json")) {
-        path1 = split1[1] + "doc" + split1[2];
-      } else {
-        path1 = split1[1];
-      }
+      path1 = path1.replace(directory.getCanonicalPath(), "");
       if (f.getPath().contains("i18n") && new File(docPath + path1).exists() && !override) {
-        LOGGER.debug("Not overwrinting :{}", docPath + path1);
+        LOGGER.debug("Not override :{}", docPath + path1);
       } else {
         LOGGER.debug("Copying {} to {}", f.getPath(), docPath + path1);
         FileUtils.copyFile(f, new File(docPath + path1));
@@ -133,11 +116,11 @@ public class GetAndDeployService {
     }
   }
 
-  public boolean verifyStructure(File toverify) {
+  private boolean verifyStructure(File toverify) {
     LOGGER.debug("Verifying :{}", toverify.getName());
     String directory = "i18n";
     String file = "multi-doc.json";
-    return (new File(toverify.getAbsolutePath() + "/doc/" + directory).exists()
-        && new File(toverify.getAbsolutePath() + "/doc/" + file).exists());
+    return (new File(toverify.getAbsolutePath() + "/" + directory).exists()
+        && new File(toverify.getAbsolutePath() + "/" + file).exists());
   }
 }
