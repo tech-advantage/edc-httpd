b45ac0cf3bbd921eb318e50c83b1a9792f55f908
gojoise <gojoise.pub@gmail.com>
2022-01-19 15:35:58 +0100

    Limit indisponibilty time during synchronization

--- src/main/java/fr/techad/edc/httpd/search/GetAndDeployService.java
+++ src/main/java/fr/techad/edc/httpd/search/GetAndDeployService.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -38,7 +39,7 @@ public class GetAndDeployService {
     } catch (IOException e) {
       LOGGER.error("Error in processing operations", e);
     }
-    removeTempFiles(tmpdir, zip);
+    removeTempFiles(tmpdir, zip,docPath);
     LOGGER.info("Processing finished");
     return succeed;
   }
@@ -53,10 +54,18 @@ public class GetAndDeployService {
       return false;
   }
 
-  private void removeTempFiles(File tmpdir, File zip) {
+  private void removeTempFiles(File tmpdir, File zip,String docPath) {
     try {
       if (tmpdir.exists()) {
         FileUtils.deleteDirectory(tmpdir);
+        LOGGER.debug("Cleaning :{}", tmpdir.getCanonicalPath());
+      }
+      File[] dirToRemove = new File(docPath).listFiles(File::isDirectory);
+      for(File f : dirToRemove) {
+        if(f.getName().contains("-old")) {
+          FileUtils.deleteDirectory(f);
+          LOGGER.debug("Cleaning :{}", f.getCanonicalPath());
+        }
       }
       zip.delete();
     } catch (IOException e) {
@@ -73,22 +82,14 @@ public class GetAndDeployService {
 
   private void cleanPreviousDoc(File directory, String docPath, boolean override) throws IOException {
     if (new File(docPath).exists()) {
-      File[] directoriestoCopy = directory.listFiles(File::isDirectory);
       File[] directoriestoClean = new File(docPath).listFiles(File::isDirectory);
-      for (File dir1 : directoriestoCopy) {
-        for (File dir2 : directoriestoClean) {
-          if (override) {
-            if (dir1.getName().equals(dir2.getName())) {// test if folders are same
-              LOGGER.debug("Cleaning :{}", dir2.getCanonicalPath());
-              FileUtils.cleanDirectory(dir2);
-            }
-          } else {
-            if (dir1.getName().equals(dir2.getName()) && !(dir2.getName().contains("i18n"))) {
-              LOGGER.debug("Cleaning :{}", dir2.getCanonicalPath());
-              FileUtils.cleanDirectory(dir2);
+        for (File dir : directoriestoClean) {
+            if (dir.getName().equals("i18n")) {
+              if (override) {
+              LOGGER.debug("Cleaning :{}", dir.getCanonicalPath());
+              FileUtils.cleanDirectory(dir);
             }
           }
-        }
       }
     }
   }
@@ -98,6 +99,7 @@ public class GetAndDeployService {
         "j2k", "jpf", "jpx", "jpm", "mj2", "tiff", "tif", "json", "svg", "svgz", "pdf", "css", "html", "png", "txt",
         "gif", "ai" };
     List<File> filestoCopy = (List<File>) FileUtils.listFiles(directory, extensions, true);
+    String name = "";
     if (override) {
       LOGGER.info("Copying new Doc with overrinding i18n");
     } else {
@@ -106,11 +108,25 @@ public class GetAndDeployService {
     for (File f : filestoCopy) {
       String path1 = f.getPath();
       path1 = path1.replace(directory.getCanonicalPath(), "");
+      path1 = path1.replace("\\", "/");
+      String [] tmp=path1.split("/");
+       name = tmp[1];
+       if(!(name.equals("multi-doc.json")||name.equals("i18n"))) {
+         tmp[1]=name+"-new";
+         path1=StringUtils.join(tmp,"/");
+       }
       if (f.getPath().contains("i18n") && new File(docPath + path1).exists() && !override) {
         LOGGER.debug("Not override :{}", docPath + path1);
       } else {
         LOGGER.debug("Copying {} to {}", f.getPath(), docPath + path1);
         FileUtils.copyFile(f, new File(docPath + path1));
+      }
+    }
+    File[] dirToMove = new File(docPath).listFiles(File::isDirectory);
+    for (File f : dirToMove) {
+        new File(f.getCanonicalPath().replace("-new", "")).renameTo(new File(f.getCanonicalPath().replace("-new", "-old"))); 
+        if(f.getName().contains("-new")){
+          f.renameTo(new File(f.getCanonicalPath().replace("-new", "")));
       }
     }
   }
