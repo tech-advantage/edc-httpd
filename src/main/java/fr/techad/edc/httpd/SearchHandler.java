2acf088388138196c66db38da1dadb855034ec18
gojoise <gojoise.pub@gmail.com>
2022-02-04 17:34:24 +0100

    fix incorrect possible values of limit

--- src/main/java/fr/techad/edc/httpd/SearchHandler.java
+++ src/main/java/fr/techad/edc/httpd/SearchHandler.java
@@ -18,53 +18,55 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * TECH ADVANTAGE
- * All right reserved
- * Created by cochon on 24/04/2018.
+ * TECH ADVANTAGE All right reserved Created by cochon on 24/04/2018.
  */
 public class SearchHandler implements HttpHandler {
-    static final Logger LOGGER = LoggerFactory.getLogger(SearchHandler.class);
-    private final ObjectMapper objectMapper;
-    private final WebServerConfig config;
+  static final Logger LOGGER = LoggerFactory.getLogger(SearchHandler.class);
+  private final ObjectMapper objectMapper;
+  private final WebServerConfig config;
 
-    public SearchHandler(ObjectMapper objectMapper, WebServerConfig config) {
-        this.objectMapper = objectMapper;
-        this.config = config;
+  public SearchHandler(ObjectMapper objectMapper, WebServerConfig config) {
+    this.objectMapper = objectMapper;
+    this.config = config;
+  }
+
+  public void handleRequest(HttpServerExchange exchange) throws Exception {
+
+    Map<String, Deque<String>> queryParameters = exchange.getQueryParameters();
+    LOGGER.debug("Query Parameters: {}, Query: {}", queryParameters, exchange.getQueryString());
+
+    Deque<String> query = queryParameters.get("query");
+    Deque<String> language = queryParameters.get("lang");
+    Deque<String> limit = queryParameters.get("limit");
+    Deque<String> strict = queryParameters.get("strict");
+    Boolean strictMode = false;
+    String lang = "";
+    int limitNumber = 100;
+    if (strict != null && language != null && limit != null) {
+      strictMode = BooleanUtils.toBoolean(strict.element());
+      lang = language.element();
+      try {
+        limitNumber = Integer.valueOf(limit.element());
+      } catch (NumberFormatException ex) {
+        LOGGER.debug("Limit is not a number, using this default limit :{}", limitNumber);
+      }
     }
 
-    public void handleRequest(HttpServerExchange exchange) throws Exception {
+    byte[] bytes;
+    if (query != null && limitNumber > 0) {
+      String search = query.element();
+      // Handle wildcard with StrictMode condition
+      if (!strictMode && !search.endsWith("*")) {
+        search = search + "*";
+      }
 
-        Map<String, Deque<String>> queryParameters = exchange.getQueryParameters();
-        LOGGER.debug("Query Parameters: {}, Query: {}", queryParameters, exchange.getQueryString());
-
-        Deque<String> query = queryParameters.get("query");
-        Deque<String> language = queryParameters.get("lang");        
-        Deque<String> limit = queryParameters.get("limit");
-        Deque<String> strict = queryParameters.get("strict");
-        Boolean strictMode = false;
-        String lang = "";
-        int limitNumber = 100;
-        if(strict != null && language != null && limit != null) {
-        strictMode = BooleanUtils.toBoolean(strict.element());
-        lang = language.element();
-        limitNumber = Integer.parseInt(limit.element());
-        }
-        
-        byte[] bytes;
-        if (query != null && limitNumber>0) {
-          String search = query.element();
-          //Handle wildcard with StrictMode condition 
-          if(!strictMode && !search.endsWith("*")) {
-            search = search+"*";
-          }
-
-            ContentSearcher contentSearcher = new ContentSearcher(config);
-            List<DocumentationSearchResult> searchResults = contentSearcher.search(search,lang,limitNumber,strictMode);
-            bytes = objectMapper.writeValueAsBytes(searchResults);
-        } else {
-            bytes = objectMapper.writeValueAsBytes(Collections.singletonMap("error", "malformed query"));
-        }
-        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
-        exchange.getResponseSender().send(ByteBuffer.wrap(bytes));
+      ContentSearcher contentSearcher = new ContentSearcher(config);
+      List<DocumentationSearchResult> searchResults = contentSearcher.search(search, lang, limitNumber, strictMode);
+      bytes = objectMapper.writeValueAsBytes(searchResults);
+    } else {
+      bytes = objectMapper.writeValueAsBytes(Collections.singletonMap("error", "malformed query"));
     }
+    exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
+    exchange.getResponseSender().send(ByteBuffer.wrap(bytes));
+  }
 }
